d0db734d0e647bdeb9b449a0cbbb8a790b0869dd
laurel vernon <laurelvernon@laurels-MBP.attlocal.net>
2020-08-05 22:56:00 -0400

    added db info

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export default {
     db: {
-      uri: 'mongodb+srv://username:test@swe-team-cluster-mt2en.mongodb.net/test?retryWrites=true&w=majority', //place the URI of your mongo database here.
+      uri: 'mongodb+srv://lvernon:citi2020@cluster0.wb4pc.mongodb.net/test?retryWrites=true&w=majority', //place the URI of your mongo database here.
     }
    };
