7a8cd13c7e6f17b1c2c0a08f7060a44d212bee31
Jenniber Franco <jenniberfranco@Jennibers-MacBook-Pro.local>
2020-08-06 23:09:07 -0600

    Edited general info page as listing page

--- client/src/views/Navigation/Subpages/GeneralInformation.js
+++ client/src/views/Navigation/Subpages/GeneralInformation.js
@@ -1,52 +1,61 @@
 import React from 'react'
 import './GeneralInformation.css'
+import { Image } from 'react-bootstrap'
 
 /*
     General information section on the navigation page. Displays static information about
-    getting to and staying at the hospital.
+    apartment listing
 */
 function GeneralInformation(props) {
     return (
         <div id="general-info-container">
-            <h1 id="navigation-info-header">General Information</h1>
+            <h1 id="navigation-info-header">Listing Details</h1>
             <div className="navigation-section starter-section">
-                <h2 className="navigation-section-header">Address</h2>
+                <h2 className="navigation-section-header">Apartment Complex: <span id="complexName">The Enclave</span></h2>
+                <h3>Seller: Ashley Walker</h3>
                 <p>
-                UF Health Neurosurgery<br/> 
+                <Image src="https://medialibrarycdn.entrata.com/media_library/10357/5cc9ee747ba9c4.98269742782.png" fluid 
+                style={{'maxHeight': '700px',"width":"50%", "marginLeft":"auto", "marginRight":"auto","display":"block"}}/>
+                <h4 style={{'maxHeight': '700px', "marginLeft":"auto", "marginRight":"auto","display":"block", "marginTop":"20px"}}>
+                    Rent: $500
+                    <span style={{'marginLeft': '200px'}}>Rooms: 3</span>
+                    <span style={{'marginLeft': '200px'}}>Bathrooms: 3</span>
+                    <p style={{'marginTop': '20px'}}>Current Roommates: 2</p>
+                </h4>
+                <br/>
+                <h3>Description</h3>
+                The Enclave's Retreat floor plan features 3 bedrooms, each with a private bathroom and walk-in closet. 
+                The home is fully furnished and includes a washer and dryer, as well as a fully equipped kitchen larger 
+                than any of our other floor plans. With granite countertops in every kitchen, you can choose one of our 
+                classic style homes to feature carpet, tile, and a black style furniture package or one of our signature 
+                suites to feature plank flooring and an upgraded furniture package with memory foam mattresses. Rent 
+                includes cable with HBO, high speed internet, parking, pest control, and emergency maintenance.
+                <br/><br/>
+                <h5>Additional Details</h5>
+                <ul>
+                    <li>Deposit: $300</li>
+                    <li>Lease Transfer Fee: $350</li>
+                    <li>Application Fee: $50</li>
+                    <li>Available By: 09/ 10/ 2020</li>
+                </ul>
+                
                 
                 <br/>
-
-                <b>
-                1505 SW Archer Road <br/>
-                Gainesville, FL 32608 <br/>
-                </b>
-
-                <br/>
-
-                UF Health Neurosurgery is located in Gainesville, Fla., 
-                on the University of Florida campus off of Archer Road (SR 24), about three miles 
-                east of I-75, exit 384, and one block west of U.S. Hwy 441.
-                </p>
-            </div>
-
-            <div className="navigation-section starter-section">
-                <h2 className="navigation-section-header">Phone</h2>
-                <p>
-                Referrals (New Patients): <b>352-273-6990</b> <br/>
-
-                Main Office: 352-273-9000
-                </p>
-            </div>
-
-            <div className="navigation-section">
-                <h2 className="navigation-section-header">Parking Information</h2>
+                <h3>Amenities</h3>
                 <ul>
-                    <li>Valet parking is available in the front circle of the hospital ($3 with a patient or patient visitor parking voucher).</li>
-                    <li>Patients and visitors must ask for a parking voucher at the check-out area, nurses’ station or other designated area when leaving their location of service and present it to the attendant upon exiting the garage.</li>
-                    <li>Please be advised that parking is cash only.</li>
-                    <li>If patients or visitors do not wish to valet, a 600-space parking garage is available adjacent to the hospital. A covered walkway from the garage leads into the lobby of the building.</li>
+                    <li>Washer & Dryer</li>
+                    <li>Private Bathroom</li>
+                    <li>Utilities Included</li>
+                    <li>Fully furnished</li>
                 </ul>
+
+                
+                </p>
             </div>
+            <img src="client/public/commSection.png" height="40px" alt=""></img>
+            <Image src="client/public/commSection.png" fluid />
+
+            
         </div>
     );
 }
